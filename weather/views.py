12c33394319af9cbe2c9bd040eda6d7bc61443c0
Ali Tahir <alitahir231@gmail.com>
2024-02-02 09:06:01 +0200

    Update to add initial WeatherView

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class WeatherView(APIView):
+    def get(self, request, city_name):
+        return Response({"message": f"We will show details for city {city_name} here!"})
