c32ecab238cb30202d12cbafcc3cdbfd3c38c875
Ali Tahir <alitahir231@gmail.com>
2024-02-02 09:39:02 +0200

    Update to implement weather data fetching from OpenWeatherMap API.

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,12 @@
+import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.conf import settings
 
 
 class WeatherView(APIView):
     def get(self, request, city_name):
-        return Response({"message": f"We will show details for city {city_name} here!"})
+        api_url = f"{settings.OPENWEATHERMAP_API_URL}?q={city_name}&appid={settings.OPENWEATHERMAP_API_KEY}&units=metric"
+        response = requests.get(api_url)
+        data = response.json()
+        return Response(data)
